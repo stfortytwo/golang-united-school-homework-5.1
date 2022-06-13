a5daf1459e9862bf5f4b4883552d241255dd36df
stfortytwo <st.forty.two@gmail.com>
2022-06-13 20:04:17 +0300

    111

--- square.go
+++ square.go
@@ -9,14 +9,17 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
-	// implement me
+func (s Square) End() Point {
+	var endPoint Point
+	endPoint.x = s.start.x + int(s.a)
+	endPoint.y = s.start.y + int(s.a)
+	return endPoint
 }
 
-func (receiver) Area() uint {
-	// implement me
+func (s Square) Area() uint {
+	return s.a * s.a
 }
 
-func (receiver) Perimeter() uint {
-	// implement me
+func (s Square) Perimeter() uint {
+	return 4 * s.a
 }
